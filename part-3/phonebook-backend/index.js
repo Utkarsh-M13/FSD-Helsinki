df22b92ac03eeb09be8e33e5618a78f37396bcf7
Utkarsh-M13 <142945737+Utkarsh-M13@users.noreply.github.com>
2025-04-26 22:43:13 -0400

    Exercise 3.22

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const morgan = require("morgan");
-require("dotenv").config();
-const Person = require("./models/person");
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+require('dotenv').config()
+const Person = require('./models/person')
+const app = express()
 
-app.use(express.json());
-app.use(express.static("dist"));
-morgan.token("body", (req, res) => {
-  return JSON.stringify(req.body);
-});
+app.use(express.json())
+app.use(express.static('dist'))
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body)
+})
 
 app.use(
   morgan((tokens, req, res) => {
@@ -16,76 +16,75 @@ app.use(
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
-      tokens["body"](req, res),
-    ].join(" ");
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+      tokens['body'](req, res),
+    ].join(' ')
   })
-);
+)
 
 // Handle known paths
 
-app.get("/api/persons", (req, res, next) => {
+app.get('/api/persons', (req, res, next) => {
   Person.find({})
     .then((people) => {
-      res.json(people);
+      res.json(people)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/api/info", (req, res, next) => {
+app.get('/api/info', (req, res, next) => {
   Person.countDocuments({})
     .then((length) => {
-      const numData = `Phonebook has data for ${length} people`;
-      const date = Date();
-      res.send(`<div>${numData}</div><div>${date}</div>`);
+      const numData = `Phonebook has data for ${length} people`
+      const date = Date()
+      res.send(`<div>${numData}</div><div>${date}</div>`)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/api/persons/:id", (req, res, next) => {
-  const params = req.params;
+app.get('/api/persons/:id', (req, res, next) => {
+  const params = req.params
   Person.findById(params.id)
     .then((person) => {
       if (person) {
-        res.json(person);
+        res.json(person)
       } else {
-        res.status(404).end();
+        res.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (req, res, next) => {
-  const params = req.params;
+app.delete('/api/persons/:id', (req, res, next) => {
 
   Person.findByIdAndDelete(req.params.id)
-    .then((result) => {
-      res.status(204).end();
+    .then(() => {
+      res.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", (req, res, next) => {
-  const name = req.body.name;
-  const number = req.body.number;
+app.post('/api/persons', (req, res, next) => {
+  const name = req.body.name
+  const number = req.body.number
 
   const person = new Person({
     name,
     number,
-  });
+  })
   person
     .save()
     .then((savedPerson) => {
-      res.json(savedPerson);
+      res.json(savedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (req, res, next) => {
-  const params = req.params;
+app.put('/api/persons/:id', (req, res, next) => {
+  const params = req.params
   Person.findByIdAndUpdate(
     params.id,
     {
@@ -95,36 +94,36 @@ app.put("/api/persons/:id", (req, res, next) => {
     {
       new: true,
       runValidators: true,
-      context: "query",
+      context: 'query',
     }
   )
     .then((savedPerson) => {
-      res.json(savedPerson);
+      res.json(savedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // Handle unknown paths
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-app.use(unknownEndpoint);
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
 // Handle errors
 const errorHandler = (error, req, res, next) => {
-  console.log("Error:", error);
+  console.log('Error:', error)
 
-  if (error.name === "CastError") {
-    return res.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return res.status(400).send({ error: error.message });
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).send({ error: error.message })
   }
 
-  next(error);
-};
-app.use(errorHandler);
+  next(error)
+}
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log("Server listening on port ", PORT);
-});
+  console.log('Server listening on port ', PORT)
+})
